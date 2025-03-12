f9c7ae23878781582cb33331c148aff30f854ce4
Jimin0430 <tangbole0430@gmail.com>
2025-03-12 20:49:58 +0900

    fix: 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"], // ✅ TMDB 이미지 도메인 허용 추가
   },
+  env: {
+    TMDB_API_KEY: process.env.TMDB_API_KEY,
+    TMDB_API_BASE_URL: process.env.TMDB_API_BASE_URL,
+    TMDB_ACCOUNT_ID: process.env.TMDB_ACCOUNT_ID,
+    TMDB_SESSION_ID: process.env.TMDB_SESSION_ID,
+    TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
